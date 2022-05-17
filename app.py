76c8108a43648cca05f58e840f82f4a9b76d18e5
yuchun <clairechen15.7@gmail.com>
2022-05-17 12:05:41 +0800

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from distutils.log import debug
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/<name>')
+
 @app.route('/')
-def index():
-    return "Hello World"
+def index(name=''):
+    return "<h1>Hello World</h1>"
 
 if __name__== '__main__':
-    app.run()
+    app.run(port=5050, debug=True)
